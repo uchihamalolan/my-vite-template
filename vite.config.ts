cbc558e8f0e33fe52757a8e3b9b21c8c41c5e760
Malolan B <uchihamalolan@gmail.com>
2022-08-31 09:22:24 +0000

    tsconfig setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import preact from '@preact/preset-vite';
 import Unocss from '@unocss/vite';
 import presetUno from '@unocss/preset-uno';
 import presetIcons from '@unocss/preset-icons';
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), Unocss({ presets: [presetUno(), presetIcons()] })],
+  plugins: [preact(), tsconfigPaths(), Unocss({ presets: [presetUno(), presetIcons()] })],
 });
