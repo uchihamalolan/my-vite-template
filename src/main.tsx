c347ac7eb1dd6d823915821b08c1265153213eaf
Malolan B <uchihamalolan@gmail.com>
2022-08-31 09:35:02 +0000

    swap preact with react

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import { render } from 'preact';
-import { App } from './app';
-import './index.css';
-import '@unocss/reset/tailwind.css';
-import 'uno.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-render(<App />, document.getElementById('app') as HTMLElement);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import react from '@vitejs/plugin-react'
 import Unocss from '@unocss/vite';
 import presetUno from '@unocss/preset-uno';
 import presetIcons from '@unocss/preset-icons';
@@ -8,5 +8,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), tsconfigPaths(), Unocss({ presets: [presetUno(), presetIcons()] })],
+  plugins: [react(), tsconfigPaths(), Unocss({ presets: [presetUno(), presetIcons()] })],
 });
